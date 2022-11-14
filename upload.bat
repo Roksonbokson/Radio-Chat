25555096858aef7a7dd411e3f0b9563b12837327
roksternad <59833831+Roksonbokson@users.noreply.github.com>
2022-11-14 14:32:25 +0100

    Update upload.bat

--- upload.bat
+++ upload.bat
@@ -1,3 +1,3 @@
 cd save destination of log file
 echo: new file detected: "%1" "%2">>log.txt 2>&1
-curl "https://@123456789@/api/v1/rooms.upload/@123@" -F file=@"%1 %2";type=audio/mpeg  -F "msg=" -F "description=" -H "X-Auth-Token: @123456789@" -H "X-User-Id: @123456789@" >>log.txt 2>&1
+curl "https://<rocket.chat server url>/api/v1/rooms.upload/<rocket.chat room id>" -F file=@"%1 %2";type=audio/mpeg  -F "msg=" -F "description=" -H "X-Auth-Token: <rocket.chat token>" -H "X-User-Id: <id of user who will post in room>" >>log.txt 2>&1
